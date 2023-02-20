ff5c4f2f031c1a6acde7beaef0b10504c17a2f39
iobox420 <iobox420@gmail.com>
2023-02-20 13:38:59 +0500

    feat: send additionInfo in sendMessage.js

--- controllers/_sendMessage.js
+++ controllers/_sendMessage.js
@@ -1,6 +1,7 @@
 const axios = require("axios")
 const models = require("../models/index.js")
 const joiChatMessageSchema = require("../models/joiChatMessageSchema.js")
+
 async function _sendMessage(req, res, next) {
   try {
     const { body } = req
@@ -10,7 +11,19 @@ async function _sendMessage(req, res, next) {
     await joiChatMessageSchema.validateAsync(body)
     const message = new models.chatmes(newFeedbackMessage)
     await message.save()
-    const text = "✅%0A Пришла новая заявка! %0A" + `Имя: ${body.name}%0A` + `Номер: ${body.telNumber}`
+    const text =
+      "✅%0A Пришла новая заявка! %0A" +
+      `Имя: ${body.name}%0A` +
+      `Телефон: ${body.telNumber}%0A` +
+      `Заголовок: ${body.currentApart.title}%0A` +
+      `Цена: ${body.currentApart.pricelocale}%0A` +
+      `Номер: ${body.currentApart.number}%0A` +
+      `Тип: ${body.currentApart.type}%0A` +
+      `Секция: ${body.currentApart.section}%0A` +
+      `Кол-во комнат: ${body.currentApart.rooms}%0A` +
+      `Площадь: ${body.currentApart.area2}%0A` +
+      `Этаж: ${body.currentApart.floor}%0A` +
+      `Доп-инфо: ${body.currentApart.additionalInfo ? body.currentApart.additionalInfo : "Пусто"}%0A`
     const messageResponce = await axios.post(
       `https://api.telegram.org/bot${process.env.TG_BOT_TOKEN}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${text}`
     )
